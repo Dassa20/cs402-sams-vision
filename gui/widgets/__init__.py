b77c56ce558e9db6dca1ba347e388f62d2f30cb2
dasun2000 <96991914+Dassa20@users.noreply.github.com>
2026-08-13 20:59:48 +0530

    update

--- gui/widgets/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Custom UI widgets for previewing vision steps and rendering Matplotlib charts."""
-
-from .preview_widget import PreviewWidget
-from .chart_widget import ChartWidget
-
-__all__ = ["PreviewWidget", "ChartWidget"]
